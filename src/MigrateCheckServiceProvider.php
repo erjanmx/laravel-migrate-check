cd233db877efb40b4fa0dcda6086f9f6e9df04c0
erjanmx-onlinesup <erjanmx@onlinesup.com>
2017-12-20 17:46:56 +0300

    Add service provider along with package autodiscovery

---
+++ src/MigrateCheckServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Erjanmx\MigrateCheck;
+
+use Illuminate\Support\ServiceProvider;
+use Erjanmx\MigrateCheck\Commands\MigrateCheckCommand;
+
+class MigrateCheckServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MigrateCheckCommand::class,
+            ]);
+        }
+    }
+}
